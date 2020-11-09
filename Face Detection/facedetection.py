d2569870e0ef2a6015e1388979adee9b90e37bbf
Ahmed Tolba <atolba210@gmail.com>
2020-11-09 16:22:52 +0200

    Add files via upload

---
+++ Face Detection/facedetection.py
@@ -0,0 +1,58 @@
+import cv2  
+  
+# Load File
+faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  
+eyesCascade = cv2.CascadeClassifier('haarcascade_eye.xml')  
+
+
+
+
+# Method to draw boundary around the detected feature
+def draw_boundary(img, classifier, scaleFactor, minNeighbors, color, text):
+    # Converting image to gray-scale
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # detecting features in gray-scale image, returns coordinates, width and height of features
+    features = classifier.detectMultiScale(gray_img, scaleFactor, minNeighbors)
+    coords = []
+    # drawing rectangle around the feature and labeling it
+    for (x, y, w, h) in features:
+        cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+        cv2.putText(img, text, (x, y-4), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 1, cv2.LINE_AA)
+        coords = [x, y, w, h]
+    return coords
+
+def detect(img, faceCascade, eyeCascade  ):
+    color = {"blue":(255,0,0), "red":(0,0,255), "green":(0,255,0), "white":(255,255,255)}
+    coords = draw_boundary(img, faceCascade, 1.1, 10, color['blue'], "Face")
+    # If feature is detected, the draw_boundary method will return the x,y coordinates and width and height of rectangle else the length of coords will be 0
+    if len(coords)==4:
+        # Updating region of interest by cropping image
+        roi_img = img[coords[1]:coords[1]+coords[3], coords[0]:coords[0]+coords[2]]
+        # Passing roi, classifier, scaling factor, Minimum neighbours, color, label text
+        coords = draw_boundary(roi_img, eyeCascade, 1.1, 12, color['red'], "Eye")
+
+
+
+        
+    return img
+
+
+
+
+# capture frames from a camera 
+video_capture = cv2.VideoCapture(0) 
+  
+while True:
+    # Reading image from video stream
+    _, img = video_capture.read()
+    # Call method we defined above
+    img = detect(img, faceCascade, eyesCascade  )
+    # Writing processed image in a new window
+    cv2.imshow("face detection", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# releasing web-cam
+video_capture.release()
+# Destroying output window
+cv2.destroyAllWindows()
